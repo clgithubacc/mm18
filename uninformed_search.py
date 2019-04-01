1e36323c7a9c1df2a56b38a4680f58087e7b3992
c <liangch0505@gmail.com>
2019-04-01 14:55:27 -0400

    Update uninformed_search.py

--- uninformed_search.py
+++ uninformed_search.py
@@ -1,36 +1,5 @@
-"""
-In uninformed_search.py, you will implement generic search algorithms 
-(using uninformed search approaches) which are called by
-Pacman agents (in searchAgents.py).
-"""
-
-import util
-from game import Directions
-def tinyMazeSearch(problem):
-    """
-    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
-    sequence of moves will be incorrect, so only use this for tinyMaze.
-    """
-    from game import Directions
-    s = Directions.SOUTH
-    w = Directions.WEST
-    return  [s, s, w, s, w, w, s, w]
 
 def depthFirstSearch1(problem):
-    """
-    Search the deepest nodes in the search tree first.
-
-    Your search algorithm needs to return a list of actions that reaches the
-    goal. Make sure to implement a graph search algorithm.
-
-    To get started, you might want to try some of these simple commands to
-    understand the search problem that is being passed in:
-
-    print "Start:", problem.getStartState()
-    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-    print "Start's successors:", problem.getSuccessors(problem.getStartState())
-    """
-    "*** YOUR CODE HERE ***"
     from game import Directions
     dir=[]
     currentState=problem.getStartState()
@@ -128,8 +97,6 @@ def depthFirstSearch(problem):
     return dir
 
 def searchPathTo(problem,start,goal):
-    """Search the shallowest nodes in the search tree first."""
-    "*** YOUR CODE HERE ***"
     print '!',
     print start,
     print goal
@@ -165,7 +132,6 @@ def searchPathTo(problem,start,goal):
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
-    "*** YOUR CODE HERE ***"
     from game import Directions
     dir=[]
     currentState=problem.getStartState()
@@ -196,7 +162,6 @@ def breadthFirstSearch(problem):
 
 def uniformCostSearch(problem,start,end):
     """Search the node of least total cost first."""
-    "*** YOUR CODE HERE ***"
     from game import Directions
     dir=[]
     currentState=problem.getStartState()
